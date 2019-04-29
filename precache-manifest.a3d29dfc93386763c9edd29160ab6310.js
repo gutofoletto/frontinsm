b712b2aa9671de5025bc43739d3ea7c6a296b140
Guto Foletto <gutofoletto@gmail.com>
2019-04-28 21:35:03 -0300

    Updates

---
+++ precache-manifest.a3d29dfc93386763c9edd29160ab6310.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b625a959ecaaa55cc5075bed8fb743d3",
+    "url": "/frontinsm/index.html"
+  },
+  {
+    "revision": "63a924797ff9b685d323",
+    "url": "/frontinsm/static/css/main.ac5a82af.chunk.css"
+  },
+  {
+    "revision": "edc02a76a66860576cce",
+    "url": "/frontinsm/static/js/2.263536ec.chunk.js"
+  },
+  {
+    "revision": "63a924797ff9b685d323",
+    "url": "/frontinsm/static/js/main.df2f14f7.chunk.js"
+  },
+  {
+    "revision": "838d2b037411a5c06553",
+    "url": "/frontinsm/static/js/runtime~main.fc255444.js"
+  },
+  {
+    "revision": "33c019b2cb87195157311bba5a7fc38b",
+    "url": "/frontinsm/static/media/home__banner.33c019b2.jpg"
+  },
+  {
+    "revision": "d3e12ebd81adaf7b99538d8a784e6a2a",
+    "url": "/frontinsm/static/media/icon-email.d3e12ebd.svg"
+  },
+  {
+    "revision": "f050fe190103fb7823199b8a7957debd",
+    "url": "/frontinsm/static/media/icon-facebook.f050fe19.svg"
+  },
+  {
+    "revision": "dca1815f6c03a764fc80990f7136e8ed",
+    "url": "/frontinsm/static/media/icon-twitter.dca1815f.svg"
+  },
+  {
+    "revision": "d891d5566447efc2c0871c8bd62087aa",
+    "url": "/frontinsm/static/media/logo.d891d556.svg"
+  }
+]);
