619e97ccb5cb523491e98c3995c73e8305b25e26
Guto Foletto <gutofoletto@gmail.com>
2019-05-08 01:24:34 -0300

    added faq styles

--- src/components/Topic.jsx
+++ src/components/Topic.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
 
+import './Topic.scss'
+
+const printList = answer => {
+  return (
+    <ul>
+      {answer.map((paragraph, index) => (
+        <li key={index}>{paragraph}</li>
+      ))}
+    </ul>
+  )
+}
+
 const Topic = ({ question, answer }) => (
   <div className='topic'>
     <p className='topic__question'>{question}</p>
-    <p className='topic__anser'>{answer}</p>
+    <p className='topic__answer'>{answer.m}</p>
+    <div className='topic__answer'>
+      {answer.length > 1 ? printList(answer) : <p>{answer}</p>}
+    </div>
   </div>
 )
 
--- src/sections/Faq.jsx
+++ src/sections/Faq.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import { Section, Container, Column } from '../components/layout'
-
-import faq from '../data/faq'
 import Topic from '../components/Topic'
 
+import faq from '../data/faq'
+
+import './Faq.scss'
+
 const Faq = () => (
-  <Section title='Perguntas Frequentes'>
-    <Container>
+  <Section title='Perguntas Frequentes' classname='faq'>
+    <Container size='small'>
       <Column size={100}>
         {faq.map((topic, index) => (
           <Topic key={index} {...topic} />
