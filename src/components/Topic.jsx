4d9f23946ed192a3825315f34b9804e761cef130
Guto Foletto <gutofoletto@gmail.com>
2019-05-08 23:09:32 -0300

    updated topic component to add link

--- src/components/Topic.jsx
+++ src/components/Topic.jsx
@@ -12,12 +12,17 @@ const printList = answer => {
   )
 }
 
-const Topic = ({ question, answer }) => (
+const Topic = ({ question, answer, link }) => (
   <div className='topic'>
     <p className='topic__question'>{question}</p>
     <p className='topic__answer'>{answer.m}</p>
     <div className='topic__answer'>
       {answer.length > 1 ? printList(answer) : <p>{answer}</p>}
+      {link && (
+        <a href={link} target='_blank' rel='noopener noreferrer'>
+          {link}
+        </a>
+      )}
     </div>
   </div>
 )
