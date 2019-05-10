9a733a6b1c8152febb4c9891fa8522a6f602dff9
Guto Foletto <gutofoletto@gmail.com>
2019-05-09 21:24:56 -0300

    fixed staff members spacing on mobile

--- src/sections/Staff.jsx
+++ src/sections/Staff.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Container, Column, Section } from '../components/layout'
+import { Container, Section } from '../components/layout'
 import { Member } from '../components'
 
 import staff from '../data/staff'
@@ -12,9 +12,7 @@ const Staff = () => (
   <Section title={title} classname='staff'>
     <Container>
       {members.map((member, index) => (
-        // <Column key={index} size={25}>
-        <Member {...member} />
-        // </Column>
+        <Member key={member.name} {...member} />
       ))}
     </Container>
   </Section>
