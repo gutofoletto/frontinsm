496431835ad2b16cdb0292cb484571a850d70bea
Guto Foletto <gutofoletto@gmail.com>
2019-05-20 21:04:26 -0300

    added code of conduct

--- src/sections/Tickets.jsx
+++ src/sections/Tickets.jsx
@@ -2,8 +2,21 @@ import React from 'react'
 import { Section, Container } from '../components/layout'
 import { Sympla } from '../components'
 
+import './Tickets.scss'
+
 const Tickets = () => (
   <Section title='Ingressos' classname='tickets'>
+    <Container size='small' align='center'>
+      <div className='code-of-conduct'>
+        <a
+          href='https://drive.google.com/open?id=1Vne8AVmqdGssuBFt0z9e7AcJfMZjefiT'
+          target='_blank'
+          rel='noopener noreferrer'
+        >
+          Código de Conduta
+        </a>
+      </div>
+    </Container>
     <Container size='small'>
       <Sympla id={461618} />
     </Container>
