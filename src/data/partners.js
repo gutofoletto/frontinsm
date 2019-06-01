21b139ef1e2b558b98e5179a3d70cc35b54d85f8
Guto Foletto <gutofoletto@gmail.com>
2019-06-01 20:11:07 -0300

    added coderace as partners

--- src/data/partners.js
+++ src/data/partners.js
@@ -1,5 +1,6 @@
 import braziljsLogo from '../images/partners/braziljs.svg'
 import frontendbrLogo from '../images/partners/frontendbr.svg'
+import codeRace from '../images/partners/coderace.svg'
 
 const partners = [
   {
@@ -11,6 +12,11 @@ const partners = [
     name: 'Front-end Brasil',
     brand: frontendbrLogo,
     url: 'https://frontendbr.com.br/',
+  },
+  {
+    name: 'Code Race',
+    brand: codeRace,
+    url: 'https://coderace.com.br/',
   },
 ]
 
