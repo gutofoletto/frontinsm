1b9b2433439da3864e74cccb1c8f590a0a126524
Guto Foletto <gutofoletto@gmail.com>
2020-02-28 09:58:39 -0300

    Updates

---
+++ precache-manifest.fbe8a3138d9cbd814647e5c740cd8f76.js
@@ -0,0 +1,142 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6069a73d47dd61a7febfdbe97722e4a4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b58b903a9d3a702e38d8",
+    "url": "/static/css/main.de5f453d.chunk.css"
+  },
+  {
+    "revision": "57aeb4091f21fe95fde3",
+    "url": "/static/js/2.58f0d668.chunk.js"
+  },
+  {
+    "revision": "b58b903a9d3a702e38d8",
+    "url": "/static/js/main.fe72e959.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "f2929db67fe06b91340a831ad43b13b1",
+    "url": "/static/media/about__bg.f2929db6.jpg"
+  },
+  {
+    "revision": "fe7025004ddb2ad9497e2240d993c8d4",
+    "url": "/static/media/debora-gasparetto.fe702500.jpg"
+  },
+  {
+    "revision": "85287fa6a42fc728b4992fc6ea40d4c4",
+    "url": "/static/media/garbin.85287fa6.svg"
+  },
+  {
+    "revision": "7efdbd386923f20b2937b83113a75444",
+    "url": "/static/media/globo.7efdbd38.svg"
+  },
+  {
+    "revision": "caadf469106530cd9af064873da63420",
+    "url": "/static/media/godaddy.caadf469.svg"
+  },
+  {
+    "revision": "7a9074df1a0228e06427f31e7ecf042d",
+    "url": "/static/media/gustavofao.7a9074df.jpg"
+  },
+  {
+    "revision": "1b473bea3620135f48583fe1ff9cc370",
+    "url": "/static/media/gutofoletto.1b473bea.jpg"
+  },
+  {
+    "revision": "2dc7ccf29031db706fd322f7f3db2429",
+    "url": "/static/media/home__banner.2dc7ccf2.jpg"
+  },
+  {
+    "revision": "5723e3f9346fcc6dd4bf28f9be100156",
+    "url": "/static/media/icon-coffee.5723e3f9.svg"
+  },
+  {
+    "revision": "c01570a6e3ce4264e75b9d048ad18195",
+    "url": "/static/media/icon-development.c01570a6.svg"
+  },
+  {
+    "revision": "07340631121029c22c5a04d7990d0f8e",
+    "url": "/static/media/icon-drink.07340631.svg"
+  },
+  {
+    "revision": "d3e12ebd81adaf7b99538d8a784e6a2a",
+    "url": "/static/media/icon-email.d3e12ebd.svg"
+  },
+  {
+    "revision": "ed75e533d4a55afbcae1ed1d2ddcef47",
+    "url": "/static/media/icon-expand.ed75e533.svg"
+  },
+  {
+    "revision": "0110d47430b8bcc708ab544f37e3d039",
+    "url": "/static/media/icon-facebook.0110d474.svg"
+  },
+  {
+    "revision": "75702fe837a03f44f5f4f122c5d25b19",
+    "url": "/static/media/icon-github.75702fe8.svg"
+  },
+  {
+    "revision": "fb259f24bb4fa6c5e966513f80a254ff",
+    "url": "/static/media/icon-lattes.fb259f24.svg"
+  },
+  {
+    "revision": "46ac2625f96c4ee7ae952a2fd88247c3",
+    "url": "/static/media/icon-medium.46ac2625.svg"
+  },
+  {
+    "revision": "90b2bd04d495abc5c555161c1a846ce5",
+    "url": "/static/media/icon-networking.90b2bd04.svg"
+  },
+  {
+    "revision": "69a8db384d5617522767a2873a43518a",
+    "url": "/static/media/icon-pizza.69a8db38.svg"
+  },
+  {
+    "revision": "cf667c3b53f0298727d311a590771363",
+    "url": "/static/media/icon-shrink.cf667c3b.svg"
+  },
+  {
+    "revision": "8b182f5e654b71511abcfcddebe06834",
+    "url": "/static/media/icon-talk.8b182f5e.svg"
+  },
+  {
+    "revision": "919928a2a101e02d9520a9db4877522b",
+    "url": "/static/media/icon-technology.919928a2.svg"
+  },
+  {
+    "revision": "dca1815f6c03a764fc80990f7136e8ed",
+    "url": "/static/media/icon-twitter.dca1815f.svg"
+  },
+  {
+    "revision": "601dde14a3b23858e7d8c419653921aa",
+    "url": "/static/media/icon-user.601dde14.svg"
+  },
+  {
+    "revision": "597771c7807c283ed997401dd4cdb894",
+    "url": "/static/media/isadora-possebon.597771c7.jpg"
+  },
+  {
+    "revision": "edbe3e8e5ab527690d99ecb9d341a760",
+    "url": "/static/media/jetimob.edbe3e8e.svg"
+  },
+  {
+    "revision": "dd85bc554e5c2d87839fe0b73b9fcb0e",
+    "url": "/static/media/logo.dd85bc55.svg"
+  },
+  {
+    "revision": "39eb4392021de3de366fc648a1f284f3",
+    "url": "/static/media/thiagoalves.39eb4392.jpg"
+  },
+  {
+    "revision": "952f351e72546f0fda372cc6f227fb79",
+    "url": "/static/media/thoughtworks.952f351e.svg"
+  },
+  {
+    "revision": "957d67414e876283e58d84e21ac5400c",
+    "url": "/static/media/tutinicola.957d6741.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/frontinsm/precache-manifest.a1e9fee9e546dabb71cc71bc94936361.js"
+  "/precache-manifest.fbe8a3138d9cbd814647e5c740cd8f76.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/frontinsm/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
