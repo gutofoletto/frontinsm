38457ac2ed1206a9ef3a5b31b8b2f43f37ef816b
Guto Foletto <gutofoletto@gmail.com>
2020-03-28 17:10:33 -0300

    remove info from 2020 edition

--- src/sections/Banner.jsx
+++ src/sections/Banner.jsx
@@ -1,31 +1,11 @@
-import './Banner.scss'
-
-import { Button } from '../components/ui'
-import { ReactComponent as Logo } from '../images/logo.svg'
 import React from 'react'
+import { ReactComponent as Logo } from '../images/logo.svg'
+import './Banner.scss'
 
 const Banner = ({ title, date, time, place, ctas }) => (
   <section className='banner'>
     <div className='banner__logo'>
       <Logo alt='Front in Santa Maria' />
-    </div>
-    <div className='banner__headline'>
-      <h1>{title}</h1>
-    </div>
-    <div className='banner__content'>
-      <h2 className='banner__title'>
-        {date} • {time}
-        <br />
-        {place}
-      </h2>
-    </div>
-    <div className='banner__cta'>
-      {ctas.length &&
-        ctas.map(({ link, text, variant }) => (
-          <Button to={link} variant={variant}>
-            {text}
-          </Button>
-        ))}
     </div>
   </section>
 )
